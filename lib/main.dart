189c62b73f582139045f1453712b4106313dc284
Rizky Maulana <breakdesign28@gmail.com>
2022-12-03 21:22:16 +0700

    net-ninja-lesson-5-scaffold-and-appbar-widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Text("Hello World")
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: const Text("First Apps"),
+        centerTitle: true,
+      ),
+      body: const Center(
+        child: Text("Welcome world"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Text("+"),
+        onPressed: () {
+        },
+        backgroundColor: Colors.blueGrey,
+      ),
+    )
   ));
 }
