684b0e574913abc90163532da18ef0c339c1ace3
Rizky Maulana <breakdesign28@gmail.com>
2022-12-03 21:44:42 +0700

    net-ninja-lesson-6-colours-&-fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,9 +6,19 @@ void main() {
       appBar: AppBar(
         title: const Text("First Apps"),
         centerTitle: true,
+        backgroundColor: Colors.blueGrey[300],
       ),
       body: const Center(
-        child: Text("Welcome world"),
+        child: Text(
+          "Welcome world",
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.amber,
+            fontFamily: 'Montserrat'
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: const Text("+"),
